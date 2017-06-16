48479e818f1d62ce24404d4f8e686f938341c7d3
Javier Fernandez Riverola <javi@MacBook-Air-de-Javier.local>
2017-06-16 23:23:34 +0200

    My reduced view

--- provision.sh
+++ provision.sh
@@ -9,30 +9,20 @@ function install_homebrew() {
 function install_brews() {
   brew tap caskroom/homebrew-cask
 
-  brews=( vim git node the_silver_searcher chruby ruby-install \
-          tmux reattach-to-user-namespace phantomjs gnu-sed \
-          rename tree wget cmake terminal-notifier irssi \
-          brew-cask siege ctags wireshark )
+  brews=( brew-cask )
 
   for item in "${brews[@]}"
   do
-    if [[ $item == "weechat" ]]; then
-      brew install $item --with-perl --with-python --with-ruby
-    elif [[ $item == "vim" ]]; then
-      brew install $item --override-system-vim
-    elif [[ $item == "wireshark" ]]; then
-      brew install wireshark --with-qt
-    else
-      brew install $item
-    fi
+    brew install $item
   done
 }
 
 function install_software() {
-  casks=( dropbox box-sync appcleaner cloudapp vlc xscope \
-          cleanmymac vienna google-chrome firefox qlcolorcode qlstephen \
-          qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql \
-          webpquicklook suspicious-package )
+  casks=( dropbox appcleaner vlc cleanmymac 1password \
+          google-chrome firefox google-hangouts \
+          sublime-text iterm2 pg-commander psequel mamp \
+          arduino fritzing \
+          spotify keka filezilla calibre skype sitesucker sysex-librarian wine-stable splice bit-torrent )
 
   for item in "${casks[@]}"
   do
@@ -40,33 +30,16 @@ function install_software() {
   done
 }
 
-function install_npm_packages() {
-  packages=( vtop is-up dnc )
-
-  for item in "${packages[@]}"
-  do
-    npm install -g $item
-  done
-}
-
-function install_php_repl() {
-  wget psysh.org/psysh
-  chmod +x psysh
-  mv ./psysh /usr/local/bin/psysh
-}
-
-function switch_to_zsh() {
-  chsh -s /bin/zsh
-}
-
-function configure_git() {
-  git config --global merge.tool vimdiff        # automatically use vimdiff
-  git config --global merge.conflictstyle diff3 # inside conflict, display a baseline
-  git config --global mergetool.prompt true     # always prompt me
+function install_app_store_software() {
+   mas signin jfernandezriverola@gmail.com
+   mas install 422304217 # Day One Classic
+   mas install 409789998 # Twitter
+   mas install 439623248 # iAWritter
+   mas install 409737246 # Double Pane
 }
 
 function install_dotfiles() {
-  git clone git@github.com:Integralist/dotfiles.git ~/dotfiles
+  git clone git@github.com:javifr/dotfiles.git ~/dotfiles
   cd ~/dotfiles && mv .*~.git ..
   cd ~/ && rm -rf dotfiles
 }
@@ -75,11 +48,6 @@ install_xcode && \
 install_homebrew && \
 install_brews && \
 install_software && \
-install_npm_packages && \
-install_php_repl && \
-switch_to_zsh && \
-configure_git && \
-install_dotfiles && \
-echo "We've installed all the software we can.
-Check the README to see if there is anything else.
-Don't forget to start Dropbox and login so it starts sync'ing files."
+install_app_store_software && \
+# install_dotfiles && \
+echo "Hecho amigo! Muchas cosas listas para ti ;)"
